4115173de54e364d0e94f4646aa402561f06b47e
kavya1230 <kavyasri.veeravalli@gmail.com>
2023-10-21 15:17:45 +0530

    Updated

--- client4.py
+++ client4.py
@@ -60,4 +60,4 @@ if __name__ == "__main__":
             prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price["ABC"], price["DEF"]))
+        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
